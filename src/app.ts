a6d8bcefde71f523c6748ce8011026d1a0f2a2e3
farjanaIslamNipa <nipa.farzana87@gmail.com>
2023-11-23 13:40:31 +0600

    User stored successfully

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { UserRoutes } from './app/modules/users/user.route';
+
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
 
 // application routes
-// app.use('/api/v1/students', StudentRoutes);
+app.use('/api/users', UserRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Mongoose Express CRUD Mastery');
